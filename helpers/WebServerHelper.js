0521b8e1d5771399f6149ce104c88e16a62231fa
Ricardo Maltez <ricardomaltez@gmail.com>
2019-03-30 23:45:29 +0000

    Fix error

--- helpers/WebServerHelper.js
+++ helpers/WebServerHelper.js
@@ -58,7 +58,7 @@ class WebServerHelper {
 
                 if (err){
                     console.error('/api/auth', err);
-                    res.sendFile(__dirname  + '/../public/error.html');
+                    res.sendFile('public/error.html', { root: __dirname + '../' });
                 }
 
                 let json = JSON.parse(body);
@@ -69,7 +69,7 @@ class WebServerHelper {
                 }
 
                 this.socketHelper.sendToRoom(token, json.access_token);
-                res.sendFile(__dirname  + '/../public/index.html');
+                res.sendFile('public/index.html',{ root: __dirname + '../' });
             });
 
         });
